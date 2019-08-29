1d2e75c519dbe126f78c07ae591ccc42b7c9e097
John Duimovich <jduimovich@gmail.com>
2019-08-29 10:42:47 -0400

    error checking

--- scripts/install_istio.sh
+++ scripts/install_istio.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-
-# Knative does not bundle Istio as of 0.6
+ 
 # Installation instructions: https://knative.dev/docs/install/installing-istio/
 
-# Download and unpack Istio
+if [ -z "$1" ]; then
+    echo "Usage ERROR need istio version"
+    exit 1
+fi
+
 export ISTIO_VERSION=$1
+if [ -d istio-${ISTIO_VERSION} ]
+then
+echo Istio  istio-${ISTIO_VERSION} already downloaded. 
+else # 
 curl -L https://git.io/getLatestIstio | sh -
+echo Downloading  istio-${ISTIO_VERSION} .
+fi
+
 cd istio-${ISTIO_VERSION}
 
 for i in install/kubernetes/helm/istio-init/files/crd*yaml; do kubectl apply -f $i; done
