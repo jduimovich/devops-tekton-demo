0d109134f3930dd32e07a2d4e207338ed88d4c02
John Duimovich <jduimovich@gmail.com>
2019-09-04 16:09:34 -0400

    automatic commit for demo

--- install.sh
+++ install.sh
@@ -102,7 +102,7 @@ else
     echo See port-forward-tekton.log for port forwarding information
     
     sh scripts/port-forward-tekton  > port-forward-tekton.log &
-    sleep 3
+    sleep 5
 fi 
 
 sh scripts/install-secret
